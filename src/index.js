2d57934bf423c0a2f2792bd181dbb24059e17ea1
Tushar161616 <sharmatushar0688@gmail.com>
2025-02-09 12:33:56 +0530

    complete

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+let typeData = new Typed(".role", {
+    strings: [
+        // "Full Stack Developer",
+        "UI/UX Designer",
+        "Web Developer",
+        "Frontend Developer",
+        // "Backend Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
