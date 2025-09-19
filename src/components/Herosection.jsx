cff67e63ee64bcf2766f06f9ad41bee2bdc2c5a8
Tushar161616 <sharmatushar0688@gmail.com>
2025-09-20 00:29:26 +0530

    again

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
 
          {/* img section  */}
         <div className="hero-img-div  h-[500px] w-[500px]  h-img">
-            <img id="hero-img" src="IMG_20220211_181310-Photoroom.png" style={{ height: "430px", width: "430px", margin: "30px", border: "8px double black", borderRadius: "50%"}}/>
+            <img id="hero-img" src={`IMG_20220211_181310-Photoroom.png`} style={{ height: "430px", width: "430px", margin: "30px", border: "8px double black", borderRadius: "50%"}}/>
             
             </div>
     </div>
